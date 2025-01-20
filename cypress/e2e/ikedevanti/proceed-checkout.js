ce1f94d42a257ff6c97baec55973cb73d0963e70
Ike Devanti <54857599+devadenti@users.noreply.github.com>
2025-01-20 20:09:44 +0700

    Update proceed-checkout.js

---
+++ cypress/e2e/ikedevanti/proceed-checkout.js
@@ -0,0 +1,33 @@
+describe('template spec', () => {
+
+
+    it('login success - success login', () => {
+      cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/')
+      cy.get('#email').type('idevanti.acc@gmail.com')
+      cy.get('#pass').type('v@nti123')
+      cy.get('#send2').click()
+    })
+
+
+
+     // choose product
+     cy.visit('https://magento.softwaretestingboard.com/gear.html')
+     cy.get('#ui-id-6')
+     cy.get('#narrow-by-list2')
+     cy.visit('https://magento.softwaretestingboard.com/gear/bags.html')
+     cy.visit('https://magento.softwaretestingboard.com/overnight-duffle.html')
+
+     // add to chart
+     cy.get('#product-addtocart-button')
+
+     //proceed checkout
+     cy.visit('https://magento.softwaretestingboard.com/checkout/cart/')
+     cy.title('Proceed to Checkout').click()
+
+
+
+    
+    
+  
+  })
+  
